3863399490a78618254a51285906d4ce7d9e659d
AdrKacz <adrien.kaczmarek@gmail.com>
2021-04-30 10:06:33 +0100

    Add easy steps to upload to server

---
+++ secure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# chmod u+x secure.sh
+echo "Secure environment"
+{
+  sudo lsof -t -i :3000 | xargs sudo kill -9
+  echo "Killed :3000"
+} || {
+  echo "Error while killing :3000"
+}
+echo "Done"
